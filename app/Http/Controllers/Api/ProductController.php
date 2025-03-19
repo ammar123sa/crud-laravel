3d478ef9c9cbf70145395fa76f2731d6a3ef2369
ammar123sa <ammaralalisa@gmail.com>
2025-03-19 14:43:35 +0300

    رسالة توضح التغييرات

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Dotenv\Validator as Dv ;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+class ProductController extends Controller
+{
+    public function index() {
+        $products = Product::get() ;
+        if($products->count() > 0)
+        {
+            return ProductResource::collection($products);
+        }
+        else{
+            return response()->json(['message' =>'No record' ],200);
+        }
+    }
+    public function store(Request $request){
+            $validator= Validator::make($request->all(),
+                [
+                    'name'=>'required|string|max:255',
+                    'description'=>'required',
+                    'price' => 'required|numeric',
+                ]);
+            if($validator->fails())
+            {
+                return response()->json(['message'=>'All fields are mandetory' ,
+                'error'=>$validator->messages()] , 422);
+            }
+            $product =Product::create([
+                'name'=>$request->name,
+                    'description'=>$request->description,
+                    'price'=>$request->price,
+            ]);
+            return response()->json(['message'=>'Product Created Successfully',
+            'data'=>new ProductResource($product)
+            ] , 200) ;
+    }
+    public function show(Product $product){
+            return new ProductResource($product) ;
+
+    }
+    public function update(Product $product, Request $request)
+{
+    // check
+    $validator = Validator::make($request->all(), [
+        'name' => 'sometimes|string|max:255',
+        'description' => 'sometimes',
+        'price' => 'sometimes|numeric',
+    ]);
+
+    // fail
+    if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation Error',
+            'error' => $validator->messages()
+        ], 422);
+    }
+
+    // update
+    $product->update($request->only(['name', 'description', 'price']));
+
+
+    return response()->json([
+        'message' => 'Product Updated Successfully',
+        'data' => new ProductResource($product)
+    ], 200);
+}
+public function destroy(Product $product)
+{
+    $product->delete() ;
+    return response()->json([
+        'message' => 'Product Deleted Successfully',200
+    ]);
+}
+}
